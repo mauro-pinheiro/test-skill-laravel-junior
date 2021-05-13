cdad02763e6da44b24ea493e1fef24d3d58e5c81
Mauro Pinheiro <mauro.pcsergio@gmail.com>
2021-05-13 19:27:57 -0300

    api routes tokens

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TokenController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProductController;
 
@@ -17,7 +18,13 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', fn(Request $request) => $request->user());
+    Route::get('/user', fn (Request $request) => $request->user());
     Route::apiResource('products', ProductController::class);
     Route::apiResource('clients', ClientController::class);
+    Route::delete('token', [TokenController::class, 'revoke'])->name('token.revoke');
+    Route::delete('token/all', [TokenController::class, 'revokeAll'])->name('token.revoke.all');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('token', [TokenController::class, 'create'])->name('token.create');
 });
