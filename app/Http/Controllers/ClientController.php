e1e9d5716e5e80cc7d5daf5383485352deab20f2
Mauro Pinheiro <mauro.pcsergio@gmail.com>
2021-05-14 08:12:03 +0000

    swagger config

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -11,28 +11,6 @@ use App\Http\Requests\ClientUpdateRequest;
 
 class ClientController extends Controller
 {
-    /**
-     * @OA\Get(
-     *      path="/projects",
-     *      operationId="getProjectsList",
-     *      tags={"Projects"},
-     *      summary="Get list of projects",
-     *      description="Returns list of projects",
-     *      @OA\Response(
-     *          response=200,
-     *          description="Successful operation",
-     *          @OA\JsonContent(ref="#/components/schemas/ProjectResource")
-     *       ),
-     *      @OA\Response(
-     *          response=401,
-     *          description="Unauthenticated",
-     *      ),
-     *      @OA\Response(
-     *          response=403,
-     *          description="Forbidden"
-     *      )
-     *     )
-     */
     public function index()
     {
         return new ClientCollection(Client::paginate());
